b1787373c4a60f87bfdc2b95768f61902d6890b4
tabaken8 <ken314ta@gmail.com>
2026-02-18 19:32:26 +0900

    search計測

--- src/lib/analytics/track.ts
+++ src/lib/analytics/track.ts
@@ -5,6 +5,7 @@ import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
 const EXCLUDED_USER_IDS = new Set<string>([
   "caab9c36-988c-4371-bbb4-360d765a35c5",
+  "fab100cb-c54b-4b0f-9d53-f05635e1e1c9",
 ]);
 
 export async function trackEvent(payload: {
